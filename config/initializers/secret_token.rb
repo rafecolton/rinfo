ae200e40987fd70524eeebc0d74564219ba81d42
Rafe Colton <r.colton@modcloth.com>
2014-03-16 23:38:43 -0700

    Oops, making rubocop happy

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,2 @@
-RinfoApp::Application.config.secret_key_base = 'QyIBwJj5mhmmU4ndOwCMD3ONFHxHNrBXF7VxJ8jz7Ge2_PBsdf'
+RinfoApp::Application.config.secret_key_base =
+  'QyIBwJj5mhmmU4ndOwCMD3ONFHxHNrBXF7VxJ8jz7Ge2_PBsdf'
